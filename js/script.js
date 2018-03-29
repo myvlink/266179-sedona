a68d94a378571002fe4607f672854db924a038a3
Vladimir Koshelev <themyvlink@gmail.com>
2018-03-30 01:00:33 +0500

    исправление ошибок

--- js/script.js
+++ js/script.js
@@ -3,5 +3,4 @@ var searchForm = document.querySelector(".search-hotel-form");
 searchButton.addEventListener("click", function (evt) {
 	evt.preventDefault();
     searchForm.classList.toggle("search-hotel-form-opened");
-    searchButton.blur();
 });
